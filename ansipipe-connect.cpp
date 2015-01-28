d950a478c9e2aba7493d3d97c6ea4fa57f35893c
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-01-28 12:58:36 +0000

    recreate ansipipe-connect C++ module based on modified C module

--- ansipipe-connect.cpp
+++ ansipipe-connect.cpp
@@ -1,60 +1,37 @@
-#include <Windows.h>
-#include <iostream>
+/*
+ * ANSI|pipe C++ connection module
+ * Redirect standard I/O through ANSI|pipe executable
+ * to enable UTF-8, ANSI escape sequences and dual mode CLI/GUI executables
+ * when packaging Python applications to a Windows executable.
+ * 
+ * based on DualModeI.cpp from dualsybsystem.
+ * GNU C++ version (c) 2015 Rob Hagemans
+ * This module is released under the terms of the MIT license.
+*/
+
+#include <windows.h>
 #include <stdio.h>
-#include "DualModeI.h"
-using namespace std;
-BOOL InitializeDualMode(BOOL initMode)
+#include <iostream>
+#include "ansipipe-connect.h"
+
+bool ansipipe_init(bool create)
 {
-  BOOL rc = FALSE;
-  //Originally these where TCHARs, but that's not very important for what we are doing
-  char szOutputPipeName[256];
-  char szInputPipeName[256];
-  char szErrorPipeName[256];
-  char achar[4] = {'a', '\0', '\0', '\0'};
-  char rchar[4] = {'r', '\0', '\0', '\0'};
+      // construct named pipe names
+      char name_out[256];
+      char name_in[256];
+      char name_err[256];
+      sprintf(name_out, "\\\\.\\pipe\\%dcout", GetCurrentProcessId());
+      sprintf(name_in, "\\\\.\\pipe\\%dcin", GetCurrentProcessId());
+      sprintf(name_err, "\\\\.\\pipe\\%dcerr", GetCurrentProcessId());
 
+      // attach named pipes to stdin/stdout/stderr
+      bool rc = false;
+      char achar[4] = "a\0\0\0";
+      char rchar[4] = "r\0\0\0";
+      rc = freopen(name_out, &achar[0], stdout) != NULL;
+      rc = rc && freopen(name_in, &rchar[0], stdin) != NULL;
+      rc = rc && freopen(name_err, &achar[0], stderr) != NULL;
 
-
-  // construct named pipe names
-  //
-  sprintf( szOutputPipeName,
-    "\\\\.\\pipe\\%dcout",
-    GetCurrentProcessId() );
-
-  sprintf( szInputPipeName,
-    "\\\\.\\pipe\\%dcin",
-    GetCurrentProcessId() );
-
-  sprintf( szErrorPipeName,
-    "\\\\.\\pipe\\%dcerr",
-    GetCurrentProcessId() );
-
-  // attach named pipes to stdin/stdout/stderr
-  //
-  rc = freopen( szOutputPipeName, &achar[0], stdout ) != NULL &&
-    freopen( szInputPipeName, &rchar[0], stdin ) != NULL &&
-    freopen( szErrorPipeName, &achar[0], stderr ) != NULL;
-
-
-  // if unsuccessfule, i.e. no console was available
-  // and initmode specifiec that we need to create one
-  // we do so
-  //
-  //Note: you may want to comment this out based on your preferences
-  if ( !rc && initMode)
-  {
-    rc = AllocConsole();
-    if (rc)
-      rc = freopen( "CONOUT$", "a", stdout ) != NULL &&
-      freopen( "CONIN$", "r", stdin ) != NULL &&
-      freopen( "CONERR$", "a", stderr ) != NULL;
-
-  }
-
-  // synchronize iostreams with standard io
-  //
-  if ( rc )
-    ios::sync_with_stdio();
-
-  return rc;
+      if (rc) ios::sync_with_stdio();
+      return rc;
 }
