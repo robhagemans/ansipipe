4fd0e7e9a3609991be6c34a3fc1ecc34f4034926
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-01-30 20:16:24 +0000

    implement minimal tty.setraw, termios.tcsetattr, termios.tcgetattr in python module

--- ansipipe.py
+++ ansipipe.py
@@ -31,3 +31,32 @@ if platform.system() == 'Windows':
         sys.stdin = sys.__stdin__
         sys.stderr = sys.__stderr__
     
+    # minimal replacements for tty.setraw() and termios.tcsa
+    # using ansipipe-only escape sequences
+    ONLCR = 4
+    ECHO = 8
+    ICRNL = 256
+    
+    termios_state = ICRNL | ECHO
+    
+    def setraw(fd, dummy=None):
+        """ Set raw terminal mode (Windows stub). """
+        tcsetattr(fd, dummy, 0)
+    
+    def tcsetattr(fd, dummy, attr):
+        """ Set terminal attributes (Windows stub). """
+        if (fd == sys.stdin.fileno()):
+            num = 254
+            sys.stdout.write('\x1b]%d;ECHO\x07' % (num + (attr & ECHO != 0)))
+            sys.stdout.write('\x1b]%d;ICRNL\x07' % (num + (attr & ICRNL != 0)))
+            sys.stdout.write('\x1b]%d;ONLCR\x07' % (num + (attr & ONLCR != 0)))
+            termios_state = attr
+    
+    def tcgetattr(fd):
+        """ Get terminal attributes (Windows stub). """
+        if (fd == sys.stdin.fileno()):
+            return termios_state
+        else:
+            return 0
+            
+    
