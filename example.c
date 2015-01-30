9b6cd32fc478148a9bf02b2e7705422301a9d178
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-01-30 11:46:00 +0000

    add example files for C, C++ and Python

---
+++ example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "ansipipe.h"
+
+int main() 
+{
+    ansipipe_init();
+    
+    printf("\x1b]2;%s\x07", "ANSI|term demo");
+    // From helloworldcollection.de. Lucida Sans doesn't support Asian scripts, but this all works:
+    printf("\n\n\n\x1b[2AHello,\x1b[1A\x1b[91;4mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92;4mмир!\x1b[0m\x1b[2B "); 
+    printf("Γεια σου \x1b[1A\x1b[94;4mκόσμε!\x1b[2B\x1b[0m\n");
+    printf("Type something: ");
+
+    char inbuf[1024];
+    fgets(inbuf, sizeof(inbuf), stdin);
+    if (strlen(inbuf) > 0) inbuf[strlen(inbuf)-1] = 0;
+    
+    printf("You typed \x1b[45;1m%s\x1b[0m.\n", inbuf);
+
+    return 0;
+}
---
+++ example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "ansipipe.hpp"
+
+int main() 
+{
+    ansipipe_init();
+    
+    std::cout << "\x1b]2;ANSI|term demo\x07";
+    // From helloworldcollection.de. Lucida Sans doesn't support Asian scripts, but this all works:
+    std::cout << "\n\n\n\x1b[2AHello,\x1b[1A\x1b[91;4mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92;4mмир!\x1b[0m\x1b[2B "; 
+    std::cout << "Γεια σου \x1b[1A\x1b[94;4mκόσμε!\x1b[2B\x1b[0m\n";
+    std::cout << "Type something: ";
+
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << "You typed \x1b[45;1m" << input << "\x1b[0m.\n";
+
+    return 0;
+}
