d8693f3147cf607aa080bed7cb20b2a570c9a2e4
Rob Hagemans <robhagemans@yahoo.co.uk>
2015-01-30 12:30:28 +0000

    don't use underline in examples as Windows console doesn't support it

--- example.c
+++ example.c
@@ -8,8 +8,8 @@ int main()
     
     printf("\x1b]2;%s\x07", "ANSI|term demo");
     // From helloworldcollection.de. Lucida Sans doesn't support Asian scripts, but this all works:
-    printf("\n\n\n\x1b[2AHello,\x1b[1A\x1b[91;4mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92;4mмир!\x1b[0m\x1b[2B "); 
-    printf("Γεια σου \x1b[1A\x1b[94;4mκόσμε!\x1b[2B\x1b[0m\n");
+    printf("\n\n\n\x1b[2AHello,\x1b[1A\x1b[91mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92mмир!\x1b[0m\x1b[2B "); 
+    printf("Γεια σου \x1b[1A\x1b[94mκόσμε!\x1b[2B\x1b[0m\n");
     printf("Type something: ");
 
     char inbuf[1024];
--- example.py
+++ example.py
@@ -3,8 +3,8 @@ import ansipipe
 
 print "\x1b]2;ANSI|term demo\x07",
 # From helloworldcollection.de. Lucida Sans doesn't support Asian scripts, but this all works:
-print "\n\n\n\x1b[2AHello,\x1b[1A\x1b[91;4mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92;4mмир!\x1b[0m\x1b[2B ",
-print "Γεια σου \x1b[1A\x1b[94;4mκόσμε!\x1b[2B\x1b[0m\n",
+print "\n\n\n\x1b[2AHello,\x1b[1A\x1b[91mWorld!\x1b[2B\x1b[0m Здравствуй,\x1b[1A\x1b[92mмир!\x1b[0m\x1b[2B ",
+print "Γεια σου \x1b[1A\x1b[94mκόσμε!\x1b[2B\x1b[0m\n",
 
 inp = raw_input("Type something: ")
 print "You typed \x1b[45;1m%s\x1b[0m.\n" % inp,
